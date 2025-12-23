f8d11870f51c8071e66e80808461052e56273385
HassanSaid21 <HassanSaeed.2020@azhar.edu.eg>
2025-12-23 10:52:23 +0200

    Time: 4 ms (48.27%), Space: 55.3 MB (93.55%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.js
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -2,21 +2,44 @@
  * @param {string} s
  * @return {boolean}
  */
-var isValid = function(s) {
-    const openTags ={'(' :')' ,
-      '{': '}','[':']'}
-    //const closedTages  = [')',']','}']
-    const validClosedTags = []
-    for (let i of s){
-        if (openTags.hasOwnProperty(i)){
-            validClosedTags.unshift(openTags[i])
-        }
-        else if(i!==validClosedTags.shift()){
-                    return false
-        }
+// var isValid = function(s) {
+//     const openTags ={'(' :')' ,
+//       '{': '}','[':']'}
+//     //const closedTages  = [')',']','}']
+//     const validClosedTags = []
+//     for (let i of s){
+//         if (openTags.hasOwnProperty(i)){
+//             validClosedTags.unshift(openTags[i])
+//         }
+//         else if(i!==validClosedTags.shift()){
+//                     return false
+//         }
        
-    }
-    if(validClosedTags.length>0)return false
-    return true
+//     }
+    
+//     return validClosedTags.length===0
      
+// };
+
+var isValid = function (s) {
+  const openTags = {
+    '(': ')',
+    '{': '}',
+    '[': ']',
+  };
+
+  const stack = [];
+
+  for (let char of s) {
+    if (Object.hasOwn(openTags, char)) {
+      stack.push(openTags[char]); // expect this closing
+    } else {
+      if (stack.pop() !== char) {
+        return false;
+      }
+    }
+  }
+
+  return stack.length === 0;
 };
+
