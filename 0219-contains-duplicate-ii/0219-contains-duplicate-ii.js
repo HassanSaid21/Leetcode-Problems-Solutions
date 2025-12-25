76fa66c76dfaed989ac8158855102133c9dd60c1
HassanSaid21 <HassanSaeed.2020@azhar.edu.eg>
2025-12-25 09:23:01 +0200

    Time: 31 ms (39.21%), Space: 85.3 MB (11.13%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.js
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.js
@@ -6,16 +6,14 @@
 var containsNearbyDuplicate = function(nums, k) {
      
     if(nums.length<=1)return false
-     
-    let hash ={}
-    for (let i =0  ;i < nums.length ; i++){
-        if(hash[nums[i]]!==undefined){
-           if( i - hash[nums[i]] <= k ) return true
-         
+    const map = new Map();
+
+    for (let i = 0; i < nums.length; i++) {
+        if (map.has(nums[i])) {
+            if (i - map.get(nums[i]) <= k) return true;
         }
-      
-            hash[nums[i]]=i
-        }
-    
-    return false
+        map.set(nums[i], i);
+    }
+
+    return false;
 };
