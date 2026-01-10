f50a992457f597f465e087b17c984e5d2dbcd91b
HassanSaid21 <HassanSaeed.2020@azhar.edu.eg>
2026-01-10 14:02:12 +0200

    Time: 0 ms (100%), Space: 53.4 MB (85.66%) - LeetHub

---
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.js
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var middleNode = function(head) {
+    let p1 = head 
+    let p2 = head 
+    while(p1 &&p1.next){
+        p1= p1.next?.next
+        p2= p2.next 
+
+    }
+    return p2
+};
