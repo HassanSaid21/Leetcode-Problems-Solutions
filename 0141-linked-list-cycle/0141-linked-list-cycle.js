3f437bbda149aed0b592facafafa45c1b097acdb
HassanSaid21 <HassanSaeed.2020@azhar.edu.eg>
2026-01-12 09:41:46 +0200

    Time: 50 ms (82.56%), Space: 60.1 MB (15.6%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.js
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+// var hasCycle = function(head) {
+//   if (!head || !head.next) return false;
+
+//   let slow = head;
+//   let fast = head;
+
+//   while (fast && fast.next) {
+//     slow = slow.next;          // move one step
+//     fast = fast.next.next;     // move two steps
+
+//     if (slow === fast) return true; // cycle detected
+//   }
+
+//   return false; // no cycle
+// };
+
+
+var hasCycle =(head)=>{
+  if(!head || !head.next) return false
+  let p1= head.next
+  let p2 = head?.next.next
+  while(p1&& p2){
+     if(p1===p2) return true
+     p1=p1.next
+     p2= p2?.next?.next
+  }
+  return false
+}
+
