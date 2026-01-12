f04d26e2af494e6cb775577220c65421d8a21b98
HassanSaid21 <HassanSaeed.2020@azhar.edu.eg>
2026-01-12 09:45:11 +0200

    Time: 53 ms (70.8%), Space: 59.5 MB (51.33%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.js
+++ 0141-linked-list-cycle/0141-linked-list-cycle.js
@@ -10,32 +10,32 @@
  * @param {ListNode} head
  * @return {boolean}
  */
-var hasCycle = function(head) {
-  if (!head || !head.next) return false;
+// var hasCycle = function(head) {
+//   if (!head || !head.next) return false;
 
-  let slow = head;
-  let fast = head;
+//   let slow = head;
+//   let fast = head;
 
-  while (fast && fast.next) {
-    slow = slow.next;          // move one step
-    fast = fast.next.next;     // move two steps
+//   while (fast && fast.next) {
+//     slow = slow.next;          // move one step
+//     fast = fast.next.next;     // move two steps
 
-    if (slow === fast) return true; // cycle detected
-  }
-
-  return false; // no cycle
-};
-
-
-// var hasCycle =(head)=>{
-//   if(!head || !head.next) return false
-//   let p1= head.next
-//   let p2 = head?.next.next
-//   while(p1&& p2){
-//      if(p1===p2) return true
-//      p1=p1.next
-//      p2= p2?.next?.next
+//     if (slow === fast) return true; // cycle detected
 //   }
-//   return false
-// }
+
+//   return false; // no cycle
+// };
+
+
+var hasCycle =(head)=>{
+  if(!head || !head.next) return false
+  let p1= head.next
+  let p2 = head?.next.next
+  while(p2&& p2.next){
+     if(p1===p2) return true
+     p1=p1.next
+     p2= p2.next.next
+  }
+  return false
+}
 
