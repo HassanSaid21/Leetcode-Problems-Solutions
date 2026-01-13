eed0635e8c5e83205ecbbb8df7e615260ed8bc1a
HassanSaid21 <HassanSaeed.2020@azhar.edu.eg>
2026-01-13 13:53:51 +0200

    Time: 7 ms (12.92%), Space: 56.3 MB (53.89%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.js
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -14,32 +14,54 @@
 //         else if(i!==validClosedTags.shift()){
 //                     return false
 //         }
-       
+
 //     }
-    
+
 //     return validClosedTags.length===0
-     
+
 // };
 
+// var isValid = function (s) {
+//   const openTags = {
+//     '(': ')',
+//     '{': '}',
+//     '[': ']',
+//   };
+
+//   const stack = [];
+
+//   for (let char of s) {
+//     if (Object.hasOwn(openTags, char)) {
+//       stack.push(openTags[char]); // expect this closing
+//     } else {
+//       if (stack.pop() !== char) {
+//         return false;
+//       }
+//     }
+//   }
+
+//   return stack.length === 0;
+// };
+
+
 var isValid = function (s) {
-  const openTags = {
-    '(': ')',
-    '{': '}',
-    '[': ']',
-  };
+    const stack = []
 
-  const stack = [];
-
-  for (let char of s) {
-    if (Object.hasOwn(openTags, char)) {
-      stack.push(openTags[char]); // expect this closing
-    } else {
-      if (stack.pop() !== char) {
-        return false;
-      }
+    for (let p of s) {
+        if (p === '(')
+            stack.push(')')
+        else if (p === '{')
+            stack.push('}')
+        else if (p === '[')
+            stack.push(']')
+        else if (p === stack[stack.length - 1])
+            stack.pop()
+        else return false
     }
-  }
 
-  return stack.length === 0;
-};
+
+    return   stack.length? false: true
+
+
+}
 
