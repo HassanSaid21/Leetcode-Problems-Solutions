eaf09ab5e6e915a61b2fbf7845d036ff9d33d344
HassanSaid21 <HassanSaeed.2020@azhar.edu.eg>
2026-01-25 17:08:34 +0200

    Time: 33 ms (56.21%), Space: 61.3 MB (26.2%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.js
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.js
@@ -3,18 +3,12 @@
  * @return {number}
  */
 var firstUniqChar = function(s) {
-    let hash = {}
-    
-    for (let i = 0 ; i<s.length;i++){
-        if(!hash[s[i]]){
-           hash[s[i]] = [i , 1]
-        }
-        else
-        hash[s[i]][1]= hash[s[i]][1]+1
-    }
+   let count = {}
+for (let c of s) count[c] = (count[c] || 0) + 1
 
-    for (let c of s){
-        if(hash[c][1]===1)return hash[c][0]
-    }
-    return -1
+for (let i = 0; i < s.length; i++){
+    if (count[s[i]] === 1) return i
+}
+return -1
+
 };
