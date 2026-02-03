1bf6a86552b992646623d968fbf1f81604bbc701
HassanSaid21 <HassanSaeed.2020@azhar.edu.eg>
2026-02-03 19:08:13 +0200

    Time: 25 ms (85.29%), Space: 66.5 MB (86.98%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.js
@@ -0,0 +1,47 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+// var groupAnagrams = function(strs) {
+//     let res =[]
+//     if (strs.length===1 ) {
+//         res[0]=[strs[0]]
+//         return res
+//     } 
+//   let j =0
+//    let hash  ={}
+//    for (let i = 0  ;  i< strs.length ;  i++){
+//    let str =  strs[i]
+//    str = str.split('').sort().join('')
+//    if(hash[str]!== undefined){
+//     res[hash[str]].push( strs[i])
+//    }
+//    else{
+//     hash[str]= j
+//     res[hash[str]]= [strs[i]]
+//     j++
+//    }
+//    console.log(hash)
+ 
+//    }
+   
+//     return res
+// };
+function groupAnagrams(strings) {
+    const anagramGroups = new Map();
+
+    for (const string of strings) {
+        const chars = Array.from(string);
+        chars.sort();
+        const canonical = chars.join('');
+
+        if (anagramGroups.has(canonical)) {
+            anagramGroups.get(canonical).push(string);
+        } else {
+            const group = [string];
+            anagramGroups.set(canonical, group);
+        }
+    }
+
+    return Array.from(anagramGroups.values());
+}
