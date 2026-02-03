81fc23210d38dba4f1e744c6152fbd16a2d53b19
HassanSaid21 <HassanSaeed.2020@azhar.edu.eg>
2026-02-03 19:10:32 +0200

    Time: 25 ms (85.29%), Space: 65 MB (98.99%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.js
+++ 0049-group-anagrams/0049-group-anagrams.js
@@ -2,46 +2,46 @@
  * @param {string[]} strs
  * @return {string[][]}
  */
-// var groupAnagrams = function(strs) {
-//     let res =[]
-//     if (strs.length===1 ) {
-//         res[0]=[strs[0]]
-//         return res
-//     } 
-//   let j =0
-//    let hash  ={}
-//    for (let i = 0  ;  i< strs.length ;  i++){
-//    let str =  strs[i]
-//    str = str.split('').sort().join('')
-//    if(hash[str]!== undefined){
-//     res[hash[str]].push( strs[i])
-//    }
-//    else{
-//     hash[str]= j
-//     res[hash[str]]= [strs[i]]
-//     j++
-//    }
-//    console.log(hash)
+var groupAnagrams = function(strs) {
+    let res =[]
+    if (strs.length===1 ) {
+        res[0]=[strs[0]]
+        return res
+    } 
+  let j =0
+   let hash  ={}
+   for (let i = 0  ;  i< strs.length ;  i++){
+   let str =  strs[i]
+   str = str.split('').sort().join('')
+   if(hash[str]!== undefined){
+    res[hash[str]].push( strs[i])
+   }
+   else{
+    hash[str]= j
+    res[hash[str]]= [strs[i]]
+    j++
+   }
+  
  
-//    }
+   }
    
-//     return res
-// };
-function groupAnagrams(strings) {
-    const anagramGroups = new Map();
+    return res
+};
+// function groupAnagrams(strings) {
+//     const anagramGroups = new Map();
 
-    for (const string of strings) {
-        const chars = Array.from(string);
-        chars.sort();
-        const canonical = chars.join('');
+//     for (const string of strings) {
+//         const chars = Array.from(string);
+//         chars.sort();
+//         const canonical = chars.join('');
 
-        if (anagramGroups.has(canonical)) {
-            anagramGroups.get(canonical).push(string);
-        } else {
-            const group = [string];
-            anagramGroups.set(canonical, group);
-        }
-    }
+//         if (anagramGroups.has(canonical)) {
+//             anagramGroups.get(canonical).push(string);
+//         } else {
+//             const group = [string];
+//             anagramGroups.set(canonical, group);
+//         }
+//     }
 
-    return Array.from(anagramGroups.values());
-}
+//     return Array.from(anagramGroups.values());
+// }
