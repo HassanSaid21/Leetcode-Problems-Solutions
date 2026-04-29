14b6d9f7339855de1ae83e6a491c46798f803219
Hassan said <HassanSaeed.2020@azhar.edu.eg>
2026-04-29 14:00:17 +0300

    Time: 27 ms (96.55%), Space: 72.7 MB (47.77%) - LeetHub

--- 0015-3sum/0015-3sum.js
+++ 0015-3sum/0015-3sum.js
@@ -1,38 +1,45 @@
-var threeSum = function(nums) {
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+function threeSum(nums) {
+    // 🔑 CRITICAL: JS `.sort()` defaults to string comparison! 
+    // Must pass a numeric comparator for correct ordering.
     nums.sort((a, b) => a - b);
+    
     const result = [];
+    const n = nums.length;
 
-    for (let i = 0; i < nums.length - 2; i++) {
-
-        // skip duplicate i
+    for (let i = 0; i < n - 2; i++) {
+        // Skip duplicate starting numbers
         if (i > 0 && nums[i] === nums[i - 1]) continue;
 
+        // Early exit: since array is sorted, if nums[i] > 0, 
+        // all subsequent triplets will sum to > 0
+        if (nums[i] > 0) break;
+
         let left = i + 1;
-        let right = nums.length - 1;
+        let right = n - 1;
 
         while (left < right) {
             const sum = nums[i] + nums[left] + nums[right];
 
-            if (sum === 0) {
+            if (sum < 0) {
+                left++;
+            } else if (sum > 0) {
+                right--;
+            } else {
                 result.push([nums[i], nums[left], nums[right]]);
-
-                // skip duplicates for left
-                while (left < right && nums[left] === nums[left + 1]) left++;
-
-                // skip duplicates for right
-                while (left < right && nums[right] === nums[right - 1]) right--;
-
                 left++;
                 right--;
-            } 
-            else if (sum < 0) {
-                left++;
-            } 
-            else {
-                right--;
+
+                // Skip duplicates for the left pointer
+                while (left < right && nums[left] === nums[left - 1]) left++;
+                // Skip duplicates for the right pointer
+                while (left < right && nums[right] === nums[right + 1]) right--;
             }
         }
     }
 
     return result;
-};
+}
