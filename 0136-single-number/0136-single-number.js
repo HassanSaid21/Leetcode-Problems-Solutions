cdc75185c77c2aa841c16d106bba3b661e2e4a1b
Hassan said <HassanSaeed.2020@azhar.edu.eg>
2026-04-30 23:50:34 +0300

    Time: 2 ms (58.45%), Space: 60.4 MB (12.82%) - LeetHub

---
+++ 0136-single-number/0136-single-number.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function(nums) {
+    
+    let set = new Set()
+    for(let n of nums){
+        if(set.has(n)){
+            set.delete(n)
+
+        }
+        else {
+            set.add(n)
+        }
+    }
+   return Array.from(set)[0]
+};
