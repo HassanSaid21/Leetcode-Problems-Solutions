5df7e8a8111fc400f4bb5e26e50b11ea0339432b
Hassan said <HassanSaeed.2020@azhar.edu.eg>
2026-05-04 19:22:30 +0300

    Time: 3 ms (49.57%), Space: 63.7 MB (10.44%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.js
+++ 0011-container-with-most-water/0011-container-with-most-water.js
@@ -3,24 +3,22 @@
  * @return {number}
  */
 var maxArea = function(height) {
-    
+    let left = 0;
+    let right = height.length - 1;
+    let maxArea = 0;
 
-    let left = 0
-    let right = height.length-1
-    let area = 0
-    while(right > left ){
-          let minHeight = height[left]>height[right]? height[right] :height[left]
-          let curArea = minHeight *( right-left)
-           area =  curArea>area? curArea :area
-        
-          if(height[right]<height[left])
+    while (left < right) {
+        const minHeight = Math.min(height[left], height[right]);
+        const currentArea = minHeight * (right - left);
+        maxArea = Math.max(maxArea, currentArea);
 
-          right--
-          else
-          left++
-
+        // Move the pointer pointing to the shorter line inward
+        if (height[left] <= height[right]) {
+            left++;
+        } else {
+            right--;
+        }
     }
 
-
-  return area 
+    return maxArea;
 };
