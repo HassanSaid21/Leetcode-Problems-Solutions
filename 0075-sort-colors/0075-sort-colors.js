eeadd7d469f3326a0b2ec9b2071804e0800b2204
Hassan said <HassanSaeed.2020@azhar.edu.eg>
2026-05-28 22:31:47 +0300

    Time: 0 ms (100%), Space: 53.7 MB (67.88%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.js
+++ 0075-sort-colors/0075-sort-colors.js
@@ -1,28 +1,47 @@
-var sortColors = function(nums) {
-    let low = 0;
-    let mid = 0;
-    let high = nums.length - 1;
+// var sortColors = function(nums) {
+//     let low = 0;
+//     let mid = 0;
+//     let high = nums.length - 1;
 
-    while (mid <= high) {
+//     while (mid <= high) {
 
-        if (nums[mid] === 0) {
+//         if (nums[mid] === 0) {
 
-            [nums[low], nums[mid]] =
-            [nums[mid], nums[low]];
+//             [nums[low], nums[mid]] =
+//             [nums[mid], nums[low]];
 
-            low++;
-            mid++;
+//             low++;
+//             mid++;
 
-        } else if (nums[mid] === 1) {
+//         } else if (nums[mid] === 1) {
 
-            mid++;
+//             mid++;
 
-        } else {
+//         } else {
 
-            [nums[mid], nums[high]] =
-            [nums[high], nums[mid]];
+//             [nums[mid], nums[high]] =
+//             [nums[high], nums[mid]];
 
-            high--;
-        }
+//             high--;
+//         }
+//     }
+// };
+
+
+
+function sortColors(nums){
+
+    let c0 = 0   , c1 = 0 , c2 = 0
+
+    for(let n of nums){
+        if(n===0)  c0++
+        if(n===1) c1++
+        if(n===2)  c2++
     }
-};
+
+    let index =  0
+    while(c0--) nums[index++] = 0
+    while(c1--) nums[index++] = 1
+    while(c2--) nums[index++] = 2
+ return nums
+}
