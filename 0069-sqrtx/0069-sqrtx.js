82718d7b1cfb844235fe373e551005c54be1339c
Hassan said <HassanSaeed.2020@azhar.edu.eg>
2026-06-08 16:10:08 +0300

    Time: 1 ms (73.44%), Space: 55.8 MB (61.47%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.js
+++ 0069-sqrtx/0069-sqrtx.js
@@ -4,9 +4,9 @@
  */
 var mySqrt = function (x) {
     if (x <= 1) return x
-    if (x === 2 || x === 3) return 1
+    // if (x === 2 || x === 3) return 1
 
-    let left = 2
+    let left = 1
     let right =  Math.floor(x/2)
 
     while (left <= right) {
