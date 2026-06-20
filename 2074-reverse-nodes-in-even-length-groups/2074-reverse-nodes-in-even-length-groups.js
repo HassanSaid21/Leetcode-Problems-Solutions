24c0e096413eb00d018372afbfbd24af586cbf64
Hassan said <HassanSaeed.2020@azhar.edu.eg>
2026-06-20 17:08:44 +0300

    Time: 6 ms (80.95%), Space: 97.7 MB (57.14%) - LeetHub

--- 2074-reverse-nodes-in-even-length-groups/2074-reverse-nodes-in-even-length-groups.js
+++ 2074-reverse-nodes-in-even-length-groups/2074-reverse-nodes-in-even-length-groups.js
@@ -1,49 +1,53 @@
-/**
- * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
- */
-/**
- * @param {ListNode} head
- * @return {ListNode}
- */
 var reverseEvenLengthGroups = function(head) {
-      if( !head || head.next ===null|| head.next.next === null) return head
-      let length= 0
-      let cur  = head
-      while(cur){
-        cur= cur.next
-        length++
-      }
-  let group=1
-     cur = head 
-    let prevGroup = head 
-    let prev= null
-      while (cur){
+    let dummy = new ListNode(0, head);
 
-        if(group%2===0){
-        let tail = cur
-        for (let i = 0 ;i<group ; i++){
-            let temp= cur.next 
-             cur.next  = prev
-             prev = cur
-             cur =  temp
-        }
-       prevGroup.next =  prev
-        tail.next = cur
-        prevGroup= tail
-        }
-        else 
-        for (let i= 0 ; i<group ;  i++){
-            
-            prevGroup =  cur
-            cur = cur.next 
+    let prevGroupTail = dummy;
+    let groupSize = 1;
 
+    while (prevGroupTail.next) {
+
+        // group start
+        let groupHead = prevGroupTail.next;
+
+        // determine actual length
+        let node = groupHead;
+        let actualLength = 0;
+
+        while (node && actualLength < groupSize) {
+            node = node.next;
+            actualLength++;
         }
-        length-= group
-        group  =  group +1 >length ? length : group+1
-      }
-      return head
+
+        // EVEN LENGTH GROUP
+        if (actualLength % 2 === 0) {
+
+            let prev = node;
+            let cur = groupHead;
+
+            for (let i = 0; i < actualLength; i++) {
+                let next = cur.next;
+
+                cur.next = prev;
+
+                prev = cur;
+                cur = next;
+            }
+
+            prevGroupTail.next = prev;
+
+            prevGroupTail = groupHead;
+        }
+
+        // ODD LENGTH GROUP
+        else {
+
+            for (let i = 0; i < actualLength; i++) {
+                prevGroupTail = prevGroupTail.next;
+            }
+        }
+
+        groupSize++;
+    }
+
+    return dummy.next;
 };
