248293e4f45bca8ab7ddbe42ee479bef274e4684
Hassan said <HassanSaeed.2020@azhar.edu.eg>
2026-07-08 18:23:51 +0300

    Time: 22 ms (39.58%), Space: 60.5 MB (85.94%) - LeetHub

---
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.js
@@ -0,0 +1,43 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var minRemoveToMakeValid = function (s) {
+
+    let res = ''
+
+    let stack1 = []
+    let stack2 = []
+    
+    for (let i= 0; i<s.length  ;   i++) {
+        if (s[i] === '(') {
+            stack1.push(i)
+
+        }
+        if (s[i] === ')') {
+            stack2.push(i)
+
+            if (stack1.length !== 0) {
+                stack2.pop()
+                stack1.pop()
+
+
+            }
+
+        }
+        
+    }
+    let set =  new Set ([...stack1 , ...stack2])
+
+    console.log(set)
+    for (let i= 0 ; i<s.length ;i++) {
+        if (!set.has(i)) {
+            res += s[i]
+        }
+        
+    }
+
+
+
+     return res
+};
