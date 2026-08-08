8fa506f15a4747e9bb57525a68cfef4da7fcd756
Hassan said <HassanSaeed.2020@azhar.edu.eg>
2026-08-08 14:04:45 +0300

    Time: 0 ms (100%), Space: 55.7 MB (68.7%) - LeetHub

--- 0766-toeplitz-matrix/0766-toeplitz-matrix.js
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.js
@@ -6,17 +6,12 @@ var isToeplitzMatrix = function(matrix) {
     
     const rows =  matrix.length
     const cols =  matrix[0].length
-    let i =0 , j= 0
-    for(i ; i<rows-1 ;i++){
-     while(j<cols-1){
-        if(matrix[i][j]!==matrix[i+1][j+1]){
-           return false
+      for (let i = 0; i < rows - 1; i++) {
+        for (let j = 0; j < cols - 1; j++) {
+            if (matrix[i][j] !== matrix[i + 1][j + 1]) {
+                return false;
+            }
         }
-            j++
-
-     } 
-
-      j=0
     }
 
 
