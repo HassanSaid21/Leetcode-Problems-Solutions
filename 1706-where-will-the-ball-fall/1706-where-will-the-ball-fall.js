41a365b78846ca8379172553ac049f684e425f2e
Hassan said <HassanSaeed.2020@azhar.edu.eg>
2026-09-10 17:00:33 +0300

    Time: 4 ms (31.25%), Space: 57.7 MB (31.25%) - LeetHub

--- 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.js
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.js
@@ -5,53 +5,47 @@
 var findBall = function (grid) {
     let rows = grid.length
     let cols = grid[0].length
-    let row , col
+    let row, col
     let answer = Array.from({ length: cols }, () => -1)
 
     for (let i = 0; i < cols; i++) {
         row = 0
         col = i
         //  we loop on each col until it get stuck or reach the end 
-        while (row < rows){
+        while (row < rows) {
             //check wether the ball in right diagonal 
             if (grid[row][col] === 1) {
                 //check if it stucks by checking on the v shape or it reach end of the wall
                 if (grid[row][col] !== grid[row][col + 1] || grid[row][col + 1] === undefined) {
                     break;
                 }
-                //move the col forward
-                // move forward the next row
-                row++
                 col++
-                // if the ball get outside  
-                // we put the output col in the index of the answer
-                if (row === rows) {
-                    answer[i] = col
-                    break
-                }
+
             }
-        //checking the left diagonal
-        if (grid[row][col] === -1) {
-            // now check if it stucks by checking on the v shape or it reach end of the wall
-            if (grid[row][col] !== grid[row][col - 1] || grid[row][col - 1] === undefined) {
-                break;
-            }
-         
-            // move forward the next row
-                row++
-                col--
-                // if the ball get outside  
-                // we put the output col in the index of the answer
-                if (row === rows) {
-                    answer[i] = col
-                    break ;
+            //checking the left diagonal
+            else {
+                // now check if it stucks by checking on the v shape or it reach end of the wall
+                if (grid[row][col] !== grid[row][col - 1] || grid[row][col - 1] === undefined) {
+                    break;
                 }
 
-        }
-       
+                col--
+            }
+
+
+            row++
+            // if the ball get outside  
+            // we put the output col in the index of the answer
+            if (row === rows) {
+                answer[i] = col
+                break;
+            }
+
+
+
 
         }
 
     }
- return answer
+    return answer
 };
