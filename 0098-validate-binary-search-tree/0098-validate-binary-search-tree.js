a435308fbb0abb81fa628029ba562c9678288595
Hassan said <HassanSaeed.2020@azhar.edu.eg>
2026-09-11 15:18:04 +0300

    Time: 0 ms (100%), Space: 59.1 MB (50.3%) - LeetHub

---
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.js
@@ -0,0 +1,23 @@
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isValidBST = function (root) {
+
+    function validate(node, min, max) {
+        if (node === null) {
+            return true;
+        }
+
+        if (node.val <= min || node.val >= max) {
+            return false;
+        }
+
+        return (
+            validate(node.left, min, node.val) &&
+            validate(node.right, node.val, max)
+        );
+    }
+
+    return validate(root, -Infinity, Infinity);
+};
