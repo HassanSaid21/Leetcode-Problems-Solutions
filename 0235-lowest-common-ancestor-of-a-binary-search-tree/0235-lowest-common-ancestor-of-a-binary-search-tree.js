550830ea247799da2608ca72da607d2b7c9abeca
Hassan said <HassanSaeed.2020@azhar.edu.eg>
2026-09-15 21:31:10 +0300

    Time: 64 ms (46.12%), Space: 65.5 MB (30.75%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.js
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.js
@@ -1,48 +1,20 @@
-/**
- * Definition for a binary tree node.
- * function TreeNode(val) {
- *     this.val = val;
- *     this.left = this.right = null;
- * }
- */
-
-/**
- * @param {TreeNode} root
- * @param {TreeNode} p
- * @param {TreeNode} q
- * @return {TreeNode}
- */
 var lowestCommonAncestor = function(root, p, q) {
-    
 
-    let stack = []
-function sBST(node , key){
-    if(node===null) return null
-            stack.push(node)
-        if(node.val===key){
-            return node
-        }
-        else if(node.val>key){
-            
-          return  sBST(node.left , key)
-        }
-        else{
-          return  sBST(node.right , key)
+    let current = root;
 
-        }
-}
- sBST(root ,p.val)
- let set=new Set(stack)
- stack= []
- sBST(root , q.val)
-  
+    while (current) {
 
- while(stack.length>0){
-    let node = stack.pop()
-    if( set.has(node))
-    return node
- }
- return root
+        if (current.val>p.val &&current.val>q.val) {
+            // go left
+                current= current.left
+        }
+        else if (current.val<p.val &&current.val<q.val) {
+            // go right
+               current = current.right 
+            }
+        else {
+            return current
+        }
+        
+    }
 };
-
-
